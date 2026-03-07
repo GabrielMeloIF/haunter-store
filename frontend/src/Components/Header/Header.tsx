698de184fe34299245242abe31ef88067b022c65
Giovanni <giovannieli2000@gmail.com>
2026-03-07 11:26:21 -0300

    'header'

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -30,19 +30,19 @@ export default function Header() {
       <div className="flex gap-10 items-center justify-center">
 
         <div className="flex items-center gap-1">
-          <BsBorderAll className="text-white " /><Link href="/" className="text-white w-27">Meus anuncios</Link>
+          <BsBorderAll className="text-white " /><Link href="/" className="text-white w-27 hover:text-[#430883] transition duration-300">Meus anuncios</Link>
         </div>
     
         <div className="flex items-center gap-1">
-          <BsEnvelope className="text-white"  /><Link href="/" className="text-white">Mensagens</Link>
+          <BsEnvelope className="text-white "  /><Link href="/" className="text-white hover:text-[#430883] transition duration-300">Mensagens</Link>
         </div>
 
         <div className="flex items-center gap-1">
-          <BsBell className="text-white " /><Link href="/" className="text-white">Notificações</Link>
+          <BsBell className="text-white  " /><Link href="/" className="text-white hover:text-[#430883] transition duration-300">Notificações</Link>
         </div>
 
         <div className="flex items-center gap-1 ">
-        <BsCart2 className="text-white " /><Link href="/" className="text-white">Carrinho</Link>
+        <BsCart2 className="text-white " /><Link href="/" className="text-white hover:text-[#430883] transition duration-300">Carrinho</Link>
       </div>
 
       </div>
@@ -50,11 +50,11 @@ export default function Header() {
 
       {/* Entrar e Carrinho */}
      
-        <div className="bg-white/50 rounded-xl px-4 py-2">
-        <Link href="/" className="text-white p-5">Entrar</Link>
-        </div>
+      <div className="bg-white/50 rounded-xl px-7 py-1 border border-transparent hover:border-[#2A0042] transition duration-300">
+        <Link href="/" className="text-white p-2 hover:text-[#430883]  transition duration-300">Entrar</Link>
+      </div>
 
-         <div className="bg-[#430883] rounded-xl flex items-center h-10 px-4 py-2">
+         <div className="bg-[#430883] rounded-xl flex items-center h-10 px-4 py-2 hover:bg-[#7317D7] transition duration-300">
           <CgAdd className="text-xl"/><Link href="/" className="text-white p-5">Anunciar</Link>
         </div>
       
