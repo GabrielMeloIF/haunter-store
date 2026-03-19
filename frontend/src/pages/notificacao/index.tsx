37b887cd123cb39df23b762480c98f316b34c77b
giovanni <giovannieli2000@gmial.com>
2026-03-19 15:02:09 -0300

    melhorzinho

--- frontend/src/pages/notificacao/index.tsx
+++ frontend/src/pages/notificacao/index.tsx
@@ -1,20 +1,21 @@
 import Header from "@/Components/Header/Header";
 import Footer from "@/Components/Footer/Footer";
 import NavBar from "@/Components/Navbar/NavBar";
+import { Icon } from "@iconify/react";
 
 export default function Notificacao() {
   return (
     
-    <div className="bg-[#303030] min-h-screen">
+    <div className="">
       <Header />
       <NavBar/>
       <div className="flex justify-center ">
-        <h1 className="text-4xl font-bold text-white mt-10 ">Notificações</h1>
+        <h1 className="text-4xl font-bold text-white ">Notificações</h1>
       </div>
 
-      <div className="flex justify-center ">
-      <div className="flex bg-gray-600 rouded rounded-2xl w-400 h-20 items-center border-2 border-white ">
-      <div className="m-3 bg-white rounded-full w-6">a</div>
+      <div className="flex justify-center mt-10">
+      <div className="flex bg-white rouded rounded-2xl w-400 h-25 items-center shadow-lg shadow-gray-500 transition-transform duration-300 hover:scale-101">
+      <div className="m-3 bg-gray-300 rounded-full w-12 h-12"><Icon icon="heroicons:user" className="text-xl" /></div>
       </div>
    </div>
     
